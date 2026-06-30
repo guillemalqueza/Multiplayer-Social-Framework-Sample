1a5486767815662b5891ef746f18b1c18fb6cedf
Guillem Alqueza Mayench <guillemalqueza@gmail.com>
2026-06-30 21:46:03 +0200

    Sample demo

--- Source/MSFSample/MSFSample.Build.cs
+++ Source/MSFSample/MSFSample.Build.cs
@@ -8,7 +8,7 @@ public class MSFSample : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "MSFCore", "MSFManager" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
